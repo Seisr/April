78281b0277c7b4f8c7f539bf6d13957e866a73ca
Bam <khanguyen1204@gmail.com>
2024-04-04 13:20:28 +0700

    feat: add forgot password detail

--- screens/ForgotPassword.jsx
+++ screens/ForgotPassword.jsx
@@ -1,8 +1,50 @@
+import { useNavigation } from "@react-navigation/native";
+import { Input } from "@rneui/base";
 import React from "react";
-import { Text } from "react-native";
+import {
+  Button,
+  Keyboard,
+  StyleSheet,
+  Text,
+  View,
+  TouchableWithoutFeedback,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const ForgotPassword = () => {
-  return <Text>Forgotpass</Text>;
+  const navigation = useNavigation();
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <SafeAreaView style={styles.login}>
+        <View style={styles.login_input}>
+          <Input placeholder="Username"></Input>
+          <Input placeholder="Email"></Input>
+          <Button
+            title="Reset Password"
+            onPress={() => navigation.navigate("Login")}
+          />
+        </View>
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
+  );
 };
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  login: {
+    flex: 1,
+    alignItem: "center",
+    justifyContent: "center",
+  },
+  login_input: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 50,
+  },
+});
 export default ForgotPassword;
